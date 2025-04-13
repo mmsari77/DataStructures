4d5dd4c57dbb090a438b72f207d98dc9da59c2a7
Sariyev Maxot <aleksandrlabincev@gmail.com>
2025-04-13 23:07:19 +0500

    Added MyNoSuchElementException and MyList interface

---
+++ src/MyNoSuchElementException.java
@@ -0,0 +1,9 @@
+public class MyNoSuchElementException extends RuntimeException {
+    public MyNoSuchElementException() {
+        super();
+    }
+
+    public MyNoSuchElementException(String message) {
+        super(message);
+    }
+}
